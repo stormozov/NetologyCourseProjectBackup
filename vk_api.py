3fe943cb6b5815bf7722ef2f9c4b1e8d78d25fad
Sergey Tormozov <web@stormozov.ru>
2024-06-17 17:49:07 +0300

    feat(api): Added a new "get_photos" method
    The GetVkPhotos class has been updated with a new get_photos method. This method allows users to retrieve information about the number of likes, date in Unix time format, and largest photo size.
    
    Refs: #2

--- vk_api.py
+++ vk_api.py
@@ -1,21 +1,18 @@
-import os
-import json
-import pprint
 import requests
 
 
 class GetVkPhotos:
-	def __init__(self, token, user_id, album_id, count=5):
+	def __init__(self, token_path, user_id, album_id='wall', count=5):
 		self.url = 'https://api.vk.com/method/photos.get'
-		self.token_path = token
+		self.token_path = token_path
 		self.params = {
-			'access_token': token,
+			'access_token': '',
 			'owner_id': user_id,
 			'album_id': album_id,
 			'count': count,
 			'extended': 1,
 			'photo_sizes': 1,
-			'v': 5.131,
+			'v': 5.199,
 		}
 
 	def get_photos_info(self):
@@ -23,6 +20,24 @@ class GetVkPhotos:
 			self.params['access_token'] = f.read().strip()
 			return requests.get(self.url, params=self.params).json()
 
+	def get_photos(self):
+		photos_info = self.get_photos_info()['response']['items']
+		photos_list = []
+		for photo in photos_info:
+			test = ''
+			for size in photo['sizes']:
+				if size['type'] == 'w':
+					test = size['url']
+				elif size['type'] == 'z':
+					test = size['url']
 
-test = GetVkPhotos('service_info/vk_token.txt', 133468233, 'profile', 12).get_photos_info()
-pprint.pprint(test)
+			photos_list.append({
+				'likes': photo['likes']['count'],
+				'date': photo['date'],
+				'sizes': test
+			})
+
+		return photos_list
+
+
+test = GetVkPhotos('service_info/vk_token.txt', 133468233, 'profile', 2).get_photos()
