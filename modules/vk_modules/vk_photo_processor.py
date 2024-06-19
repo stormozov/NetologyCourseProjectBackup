65727f5bcd68dbcbeebad8db74fc96b8b473662f
Sergey Tormozov <web@stormozov.ru>
2024-06-19 19:15:41 +0300

    refactor(VK): Add "_extract_photo_info" method
    The "_extract_photo_info" method has been added to the "VKPhotoProcessor" class. This method creates a filename based on the number of likes and the date that the photo was taken.
    
    Refs: #1, #2, #4

--- modules/vk_modules/vk_photo_processor.py
+++ modules/vk_modules/vk_photo_processor.py
@@ -8,14 +8,24 @@ class VKPhotoProcessor:
 			sizes = photo.get('sizes', [])
 			likes_count = photo['likes'].get('count', 0) if 'likes' in photo else 0
 			date = UnixToDate(photo['date'], date_format).convert()
+			filename = self._create_filename(photo, photo['likes']['count'], date, photo_list)
 			size_url, size_type = self._find_preferred_size_url(sizes, preferred_size)
 			photo_list.append({
 				'likes': likes_count,
 				'date': date,
 				'size_type': size_type,
+				'filename': f'{filename}.jpg',
 				'url': size_url
 			})
 		return photo_list
 
 	def _find_preferred_size_url(self, sizes: list, preferred_size: str = 'z'):
 		return next(([size['url'], size['type']] for size in sizes if size['type'] in preferred_size), None)
+
+	def _create_filename(self, photo, likes, date, photo_list) -> str:
+		filename = f"{likes}"
+		for item in photo_list:
+			if item["likes"] == likes and item != photo:
+				filename += f"_{date}"
+				break
+		return filename
--- vk_api.py
+++ vk_api.py
@@ -4,43 +4,44 @@ from modules.vk_modules.vk_photo_processor import VKPhotoProcessor
 
 
 class VkProfilePhotosRetriever(VKPhotoProcessor):
-    config = configparser.ConfigParser()
-    config.read('settings.ini')
-    vk_api = config['VK_API']
-    API_VERSION: int = vk_api['Api_version']
-    URL: str = vk_api['Url']
-    TOKEN: str = vk_api['Token']
-    DATE_FORMAT: str = '%Y-%m-%d'
-    PREFERRED_SIZES = ['w', 'z']
+	config = configparser.ConfigParser()
+	config.read('settings.ini')
+	vk_api = config['VK_API']
+	API_VERSION: int = vk_api['Api_version']
+	URL: str = vk_api['Url']
+	TOKEN: str = vk_api['Token']
+	DATE_FORMAT: str = '%Y-%m-%d'
+	PREFERRED_SIZES = ['w', 'z']
 
-    def __init__(self, method: str, user_id: int, album_id: str = 'wall', count: int = 5) -> None:
-        self.method = method
-        self.params = {
-            'access_token': self.TOKEN,
-            'owner_id': user_id,
-            'album_id': album_id,
-            'count': count,
-            'extended': 1,
-            'photo_sizes': 1,
-            'v': self.API_VERSION,
-        }
+	def __init__(self, method: str, user_id: int, album_id: str = 'wall', count: int = 5) -> None:
+		self.method = method
+		self.params = {
+			'access_token': self.TOKEN,
+			'owner_id': user_id,
+			'album_id': album_id,
+			'count': count,
+			'extended': 1,
+			'photo_sizes': 1,
+			'v': self.API_VERSION,
+		}
 
-    def retrieve_photo_data(self):
-        try:
-            response = requests.get(self.URL + self.method, params=self.params)
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            print(f"Error during HTTP request: {e}")
-            return None
+	def retrieve_photo_data(self):
+		try:
+			response = requests.get(self.URL + self.method, params=self.params)
+			return response.json()
+		except requests.exceptions.RequestException as e:
+			print(f"Error during HTTP request: {e}")
+			return None
 
-    def get_photos(self):
-        response = self.retrieve_photo_data()
-        if 'response' in response and 'items' in response['response']:
-            photos = response['response']['items']
-            photo_list = self._extract_photo_info(photos, self.DATE_FORMAT, self.PREFERRED_SIZES)
-            return photo_list
-        else:
-            return []
+	def get_photos(self):
+		response = self.retrieve_photo_data()
+		if 'response' in response and 'items' in response['response']:
+			photos = response['response']['items']
+			photo_list = self._extract_photo_info(photos, self.DATE_FORMAT, self.PREFERRED_SIZES)
+			return photo_list
+		else:
+			return []
 
 
-test = VkProfilePhotosRetriever('photos.get', 133468233, 'profile', 2).get_photos()
+test = VkProfilePhotosRetriever('photos.get', 133468233, 'profile', 12)
+result = test.get_photos()
