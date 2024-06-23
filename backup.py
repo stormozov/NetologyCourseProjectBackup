2ad671a2b1059446aa09dbc6017d406f439ab2ff
Sergey Tormozov <web@stormozov.ru>
2024-06-23 21:18:55 +0300

    refactor: Yandex.Disk and VK Authentication
    1. The Yandex.Disk authentication token has been added to the configuration file to improve data security.
    2. Constants have been declared in modules to improve their interaction.
    3. Annotations have been added

--- backup.py
+++ backup.py
@@ -1,22 +1,34 @@
+import configparser
 from modules.ya_disk_upload.ya_disk_uploader import YaDiskUploader
 from modules.vk_modules.vk_api import VkProfilePhotosRetriever
 from modules.create_json.create_json import create_json_file
 
+config = configparser.ConfigParser()
+config.read('settings.ini')
+VK_TOKEN, YA_DISK_TOKEN = config['VK_API']['token'], config['YANDEX_DISK']['token']
+
 
 class Backup:
-	def __init__(self, user_id: int, album_id: str = 'profile', count: int = 5) -> None:
-		data = VkProfilePhotosRetriever('photos.get', user_id, album_id, count)
+	def __init__(
+			self, vk_token: str, ya_disk_token: str,
+			user_id: int, album_id: str = 'profile', count: int = 5
+	) -> None:
+		data = VkProfilePhotosRetriever(vk_token, 'photos.get', user_id, album_id, count)
+		self.ya_disk_token = ya_disk_token
 		self.full_info = data.get_photos_info()['full_info']
 		self.json = data.get_photos_info()['json']
 
 	def upload(self, album_id: str = 'profile') -> None:
-		ya_disk_uploader = YaDiskUploader(self.full_info)
+		ya_disk_uploader = YaDiskUploader(self.ya_disk_token, self.full_info)
 		ya_disk_uploader.create_folder_process(album_id)
 
 	def create_json(self) -> None:
 		create_json_file(self.json)
 
 
-backup_photos_from_vk = Backup(133468233, 'profile', 12)
+backup_photos_from_vk = Backup(
+	VK_TOKEN, YA_DISK_TOKEN,
+	133468233, 'profile', 12
+)
 backup_photos_from_vk.upload()
 # backup_photos_from_vk.create_json()
--- modules/vk_modules/vk_api.py
+++ modules/vk_modules/vk_api.py
@@ -1,35 +1,35 @@
-import configparser
 from modules.fetch_photos_info.fetch_photos_info import fetch_photo_data
 from modules.vk_modules.vk_photo_processor import VKPhotoProcessor
 
-class VkProfilePhotosRetriever(VKPhotoProcessor):
-	config = configparser.ConfigParser()
-	config.read('settings.ini')
-	vk_api = config['VK_API']
-	API_VERSION: int = vk_api['Api_version']
-	URL: str = vk_api['Url']
-	TOKEN: str = vk_api['Token']
-	DATE_FORMAT: str = '%Y-%m-%d'
-	PREFERRED_SIZES = ['w', 'z']
+API_VERSION: float | int = 5.199
+URL: str = 'https://api.vk.com/method/'
+DATE_FORMAT: str = '%Y-%m-%d'
+PREFERRED_SIZES: str | list = ['w', 'z']
 
-	def __init__(self, method: str, user_id: int, album_id: str = 'wall', count: int = 5) -> None:
-		self.method = method
-		self.params = {
-			'access_token': self.TOKEN,
+
+class VkProfilePhotosRetriever(VKPhotoProcessor):
+	def __init__(
+			self, token: str, method: str, user_id: int,
+			album_id: str = 'wall', count: int = 5
+	) -> None:
+		self.method: str = method
+		self.params: dict = {
+			'access_token': token,
 			'owner_id': user_id,
 			'album_id': album_id,
 			'count': count,
 			'extended': 1,
 			'photo_sizes': 1,
-			'v': self.API_VERSION,
+			'v': API_VERSION,
 		}
 
 	def get_photos_info(self):
-		data = fetch_photo_data(self.URL, self.method, self.params)
+		data = fetch_photo_data(URL, self.method, self.params)
+
 		if 'response' in data and 'items' in data['response']:
 			photos = data['response']['items']
 			extracted_photo_info = self._extract_photo_info(
-				photos, self.DATE_FORMAT, self.PREFERRED_SIZES
+				photos, DATE_FORMAT, PREFERRED_SIZES
 			)
 			return extracted_photo_info
 		else:
