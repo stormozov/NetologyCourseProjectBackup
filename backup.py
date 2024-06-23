2e7d5b8b7ee005abd51003080620c29289d3de06
Sergey Tormozov <web@stormozov.ru>
2024-06-23 16:45:33 +0300

    refactor(uploader): The "YaDiskUploader" class was improved
    - The session from the requests library was used to automatically send request headers for each request.
    - A new "create_type_folder" method has been added, which creates an additional folder based on the type of photos that are uploaded.
    
    Refs: #9

--- backup.py
+++ backup.py
@@ -4,16 +4,16 @@ from modules.create_json.create_json import create_json_file
 
 
 class Backup:
-	def __init__(self, user_id: int, album_id: str = 'profile', count: int = 5):
+	def __init__(self, user_id: int, album_id: str = 'profile', count: int = 5) -> None:
 		data = VkProfilePhotosRetriever('photos.get', user_id, album_id, count)
 		self.full_info = data.get_photos_info()['full_info']
 		self.json = data.get_photos_info()['json']
 
-	def upload(self):
+	def upload(self, album_id: str = 'profile') -> None:
 		ya_disk_uploader = YaDiskUploader(self.full_info)
-		ya_disk_uploader.create_folder_process()
+		ya_disk_uploader.create_folder_process(album_id)
 
-	def create_json(self):
+	def create_json(self) -> None:
 		create_json_file(self.json)
 
 
--- modules/ya_disk_upload/ya_disk_uploader.py
+++ modules/ya_disk_upload/ya_disk_uploader.py
@@ -8,54 +8,80 @@ class YaDiskUploader:
 		}
 		self.url = 'https://cloud-api.yandex.net/v1/disk/resources'
 		self.root_folder_name = 'Бэкап фото из ВК'
+		self.type_folder_name = ''
 		self.photos_list = photos
-		self.date_format = '%d %B %Y'
+		self.session = requests.Session()
+		self.session.headers.update(self.headers)
 
-	def create_folder_process(self):
+	def create_folder_process(self, album_id: str) -> None:
 		self._create_root_folder()
-		self._create_folder()
+		self._create_type_folder(album_id)
+		self._create_backup_folder(self.type_folder_name)
 
 	def _create_root_folder(self):
-		params = {'path': self.root_folder_name}
-		response = requests.get(self.url, headers=self.headers, params=params)
+		params: dict[str, str] = {'path': self.root_folder_name}
+		response = self.session.get(self.url, params=params)
 
 		if response.status_code == 404:
-			requests.put(self.url, headers=self.headers, params=params)
+			self.session.put(self.url, params=params)
 			print(f'Создана корневая папка резервных копий "{self.root_folder_name}"')
 
-	def _create_folder(self):
-		root_folder_params = {'path': f'{self.root_folder_name}'}
-		create_folder_response = requests.get(self.url, headers=self.headers, params=root_folder_params)
+	def _create_type_folder(self, album_id: str):
+		folder_mapping = {
+			'profile': 'Фото профиля',
+			'wall': 'Фото со стены',
+			'saved': 'Сохраненные фото',
+			'default': 'Альбом'
+		}
+
+		self.type_folder_name = folder_mapping.get(album_id, 'default')
+
+		params = {'path': f'{self.root_folder_name}/{self.type_folder_name}'}
+
+		try:
+			response = self.session.get(self.url, params=params)
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as e:
+			if e.response.status_code == 404:
+				self.session.put(self.url, params=params)
+				print(f'Создана папка "{folder_mapping.get(album_id, "default")}"')
+			else:
+				raise
+
+	def _create_backup_folder(self, folder_type: str):
+		root_folder_params: dict[str, str] = {'path': f'{self.root_folder_name}/{folder_type}'}
+		create_folder_response = self.session.get(self.url, params=root_folder_params)
 		json_response = create_folder_response.json()['_embedded']['items']
 
 		if not json_response:
-			params = {'path': f'{self.root_folder_name}/Резервная копия №1'}
-			request = requests.put(self.url, headers=self.headers, params=params)
+			params = {'path': f'{self.root_folder_name}/{folder_type}/Резервная копия №1'}
+			request = self.session.put(self.url, params=params)
 			if request.status_code == 201:
 				print(f'Создана папка "Резервная копия №1"')
-				self._upload_photos('Резервная копия №1')
+				self._upload_photos(folder_type, 'Резервная копия №1')
 		else:
 			count = 2
 			while True:
-				params = {'path': f'{self.root_folder_name}/Резервная копия №{count}'}
-				request = requests.put(self.url, headers=self.headers, params=params)
+				params = {'path': f'{self.root_folder_name}/{folder_type}/Резервная копия №{count}'}
+				request = self.session.put(self.url, params=params)
 
 				if request.status_code == 201:
 					print(f'Создана папка "Резервная копия №{count}"')
 					break
 				count += 1
 
-			self._upload_photos(f'Резервная копия №{count}')
+			self._upload_photos(folder_type, f'Резервная копия №{count}')
 
-	def _upload_photos(self, folder_name: str):
+	def _upload_photos(self, folder_type, folder_name: str):
 		for idx, photo in enumerate(self.photos_list):
-			upload_params = {
+			upload_params: dict[str, str] = {
 				'url': photo['url'],
-				'path': f'{self.root_folder_name}/{folder_name}/{photo['filename']}'
+				'path': f'{self.root_folder_name}/{folder_type}/{folder_name}/{photo['filename']}'
 			}
-			upload = requests.post(f'{self.url}/upload', headers=self.headers, params=upload_params)
-			if upload.status_code == 202:
-				print(f'{idx + 1}. Фото "{photo["filename"]}" загружено')
-			else:
-				print(f'Фото "{photo["filename"]}" не загружено')
+			try:
+				upload = self.session.post(f'{self.url}/upload', params=upload_params)
+				upload.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+				print(f"{idx + 1}. Фото {photo['filename']} загружено")
+			except requests.exceptions.HTTPError as e:
+				print(f"Фото {photo['filename']} не загружено: {e}")
 		print(f'— Все {len(self.photos_list)} фото загружены')
